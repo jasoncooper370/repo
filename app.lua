00e82ebf911e3ab0d15cbfb04254de2ce041c5c0
codemaster <dominiclau0821@gmail.com>
2026-06-14 17:53:47 +0100

    Add files via upload

---
+++ app.lua
@@ -0,0 +1,8 @@
+local window = require("window")
+local document = window.document
+ 
+local textFieldFrame = document:getElementById("loginFormFrame")
+local passwordFormFrame = document:getElementById("passwordFormFrame")
+
+local usernameField = document:getElementById("usernameInput")
+local passwordField = document:getElementById("passwordInput")
